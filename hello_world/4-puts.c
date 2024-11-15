c519c0a350d11b32028b1839e8a269b6bf3f4560
Tofigkh1 <khaniyef01@gmail.com>
2024-11-15 10:23:50 +0000

    test

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
 
-
 /**
  * main - Entry point
  *
- * C puts fonctions
+ * Description: Prints "Programming is like building a multilingual puzzle"
+ * using the puts function.
+ *
+ * Return: Always 0 (Success)
  */
-
-int main () {
-
-
-puts("\"Programming is like building a multilingual puzzle");
-
-
-    return 0;
-
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
