0167553fc509b0431f185b0af814b24c35670239
Tofigkh1 <khaniyef01@gmail.com>
2024-11-15 10:30:02 +0000

    test

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,13 +3,15 @@
 /**
  * main - Entry point
  *
- * Description: Prints "Programming is like building a multilingual puzzle"
+ * Description: Prints.
  * using the puts function.
  *
- * Return: Always 0 (Success)
+ * Return: Always
  */
+
+
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
