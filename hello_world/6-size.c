c7f6c464898059cb20e77cfb27285c9d80ec88ba
Tofigkh1 <khaniyef01@gmail.com>
2024-11-15 11:00:55 +0000

    test

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,33 +1,20 @@
 #include <stdio.h>
 
-
 /**
  * main - Entry point of the program
  *
- * This program prints the size (in bytes) of various data types in C.
- * It includes basic types like char, int, float, and double, as well
- * as their variations such as unsigned, signed, long, and short types.
- * The sizeof operator is used to determine the memory size of each type.
+ * This program prints the size (in bytes) of specific data types in C.
+ * It uses the sizeof operator to determine the memory size of each type.
  *
  * Return: Always (Success)
  */
 
-
-
-int main () {
-printf("char: %zu byte(s)\n", sizeof(char));
-printf("unsigned char: %zu byte(s)\n", sizeof(unsigned char));
-printf("signed char: %zu byte(s)\n", sizeof(signed char));
-printf("short: %zu byte(s)\n", sizeof(short));
-printf("unsigned short: %zu byte(s)\n", sizeof(unsigned short));
-printf("int: %zu byte(s)\n", sizeof(int));
-printf("unsigned int: %zu byte(s)\n", sizeof(unsigned int));
-printf("long: %zu byte(s)\n", sizeof(long));
-printf("unsigned long: %zu byte(s)\n", sizeof(unsigned long));
-printf("long long: %zu byte(s)\n", sizeof(long long));
-printf("unsigned long long: %zu byte(s)\n", sizeof(unsigned long long));
-printf("float: %zu byte(s)\n", sizeof(float));
-printf("double: %zu byte(s)\n", sizeof(double));
-printf("long double: %zu byte(s)\n", sizeof(long double));
+int main(void)
+{
+printf("Size of a char: %zu byte(s)\n", sizeof(char));
+printf("Size of an int: %zu byte(s)\n", sizeof(int));
+printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %zu byte(s)\n", sizeof(float));
 return 0;
 }
