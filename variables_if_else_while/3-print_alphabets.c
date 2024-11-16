0e42e21034262acb2c04cc7b398aa1d692808c46
Tofigkh1 <khaniyefdevelop@gmail.com>
2024-11-16 20:35:50 +0000

    test

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints the last digit of a randomly generated number
+ *        and evaluates its properties.
+ *
+ * Return: Always (Success)
+ */
+
+
+int main(void)
+{
+char c;
+for (c='A'; c<='Z'; c++){
+putchar(c);
+}
+putchar('\n');
+for (c='a'; c<='z'; c++){
+putchar(c);
+}
+return (0);
+}
