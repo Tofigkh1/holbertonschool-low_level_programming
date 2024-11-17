3a759628844d6ef58b40160d3d56dadd3862e3e8
Tofigkh1 <khaniyefdevelop@gmail.com>
2024-11-17 15:11:55 +0000

    test

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,21 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - Prints all the numbers of base 16 in lowercase.
+ * main - Prints all single digit numbers of base 10 starting from 0,
+ *        followed by a new line.
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-char c;
-for (c = '0'; c <= '9'; c++)
+int num;
+for (num = 0; num < 10; num++)
 {
-putchar(c);
-}
-for (c = 'a'; c <= 'f'; c++)
-{
-putchar(c);
+putchar(num + '0');
 }
 putchar('\n');
 return (0);
