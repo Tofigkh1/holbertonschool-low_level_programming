1610f0aff58a7adbd8fb63a6641355d64d1d9ac0
Tofigkh1 <khaniyefdevelop@gmail.com>
2024-11-17 16:04:33 +0000

    test

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints the last digit of a randomly generated number
+ *        and evaluates its properties.
+ *
+ * Return: Always (Success)
+ */
+
+
+int main(void)
+{
+char c;
+for (c = 'z'; c >= 'a'; c--)
+{
+putchar(c);
+}
+return (0);
+}
