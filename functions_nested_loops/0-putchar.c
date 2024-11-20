d92780a0a8bc512658c028e77e4633d7055a1048
Tofigkh1 <143221445+Tofigkh1@users.noreply.github.com>
2024-11-20 23:07:32 +0400

    test

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Description: Prints "_putchar" followed by a new line
+ * Descript6++ion: Prints "_putchar" followed by a new line
  * Return: Always 0 (Success)
  */
 int main(void)
---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the alphabet in lowercase, followed by a new line
+ */
+
+
+ void print_alphabet (void) {
+ char alphabet[] = "abcdefghijklmnopqrstuvwxyz\n";
+
+ _putchar(alphabet[0]);
+
+ _putchar(alphabet[1]);
+
+ }
