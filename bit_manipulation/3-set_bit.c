a98a137758fcf32bf04b932d8af48653bae6ec3a
Tofig Ganiyev <143221445+Tofigkh1@users.noreply.github.com>
2024-12-09 07:49:02 +0400

    Update 3-set_bit.c

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -12,11 +12,10 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-		if (index < sizeof(n) * 8)
-				{
-							*n = *n | (1 << index);
-									return (1);
-										}
-
-			return (-1);
+if (index < sizeof(n) * 8)
+{
+*n = *n | (1 << index);
+return (1);
+}
+return (-1);
 }
